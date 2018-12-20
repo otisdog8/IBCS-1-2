32b8b65c9c637a92749344567f786347adf6ac17
Jacob/Otis Root <otisdodo@gmail.com>
2018-12-20 11:17:29 -0800

    Finished lab 4

--- 4/lab4/src/com/company/Main.java
+++ 4/lab4/src/com/company/Main.java
@@ -1,13 +1,12 @@
 package com.company;
-
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class Main {
     static Scanner input = new Scanner(System.in); //Makes it so that everyone can use this Scanner
 
     public static void main(String[] args) {
         int response;
-        String[] items = {"End Program", "Add it up", "Grading", "Reciprocal", "LCM and GCD0"}; //Menu code
+        String[] items = {"End Program", "Add it up", "Grading", "Reciprocal", "LCM and GCD"}; //Menu code
         String menu = makemenu(items);
         do {
             System.out.println(menu);
@@ -17,9 +16,9 @@ public class Main {
             } else if (response == 2) {
                 grading();
             } else if (response == 3) {
-                additup();
+                reciprocal();
             } else if (response == 4) {
-                additup();
+                LCMGCD();
             } else {
             }
 
@@ -27,7 +26,7 @@ public class Main {
         System.out.println("Bye!");
     }
 
-    public static String makemenu(String[] items) { //Function generates a menu string so that I don't have to type it out.
+    private static String makemenu(String[] items) { //Function generates a menu string so that I don't have to type it out.
         String result = "";
         int length = items.length; //*very* slight optimization
         for (int i = 0; i < length; i++) { //Iterates through the array items and generates a menu entry for each item on items
@@ -36,7 +35,7 @@ public class Main {
         return result;
     }
 
-    public static void additup() {
+    private static void additup() {
         System.out.println("Enter an integer");
         int lastnum = input.nextInt();
         int result = 0;
@@ -46,7 +45,7 @@ public class Main {
         System.out.println("Your result is " + result);
     }
 
-    public static void grading() {
+    private static void grading() {
     int grade;
     long[] letters = new long[] {0,0,0,0,0};
     double average = 1;
@@ -79,5 +78,153 @@ public class Main {
             System.out.println("------------------------------");
         }
     } while (grade != -1);
-}
+    }
+    private static void reciprocal() {
+        int num;
+        ArrayList<Integer> primes = new ArrayList<>();
+        primes.add(2);
+        primes.add(3); //Starting prime numbers
+        ArrayList<Integer> numpfactors = new ArrayList<>(); //Variables store the prime factors
+        ArrayList<Integer> sumtoppfactors = new ArrayList<>();
+        ArrayList<Integer> sumbottomfactors = new ArrayList<>(); //Top and bottom of the fraction
+        ArrayList<Integer> commonfactors = new ArrayList<>();
+        ArrayList<Integer> additionfactors = new ArrayList<>();
+        int sumbottom = 0;
+        int sumtop = 0;
+        int lowest = 0;
+        int addition = 0;
+        int suminaddition = 0;
+        int numinaddition = 0;
+        int times = 0;
+        do {
+            times++;
+            System.out.println("Enter a number (0 to stop)");
+            num = input.nextInt();
+            if (num != 0) {
+                while (num*sumbottom > primes.get(primes.size()-1) || num*sumtop > primes.get(primes.size()-1)) {
+                    primes = primefinder(primes);
+                }
+                if (sumbottom != 0) {
+                    sumtop = sumtop * num + sumbottom; //Adds the two fractions together
+                    sumbottom = num*sumbottom;
+                    sumbottomfactors = getprimefactors(primes, sumbottom);
+                    sumtoppfactors = getprimefactors(primes, sumtop); //Creates prime factors for use in calculation
+                    for (int i = 0; i < primes.size(); i++) {
+                        lowest = Math.min(sumbottomfactors.get(i), sumtoppfactors.get(i)); //Removes the GCD from both numbers because I'm too dumb to remember that their's a build in method for GCD
+                        sumbottomfactors.set(i, sumbottomfactors.get(i) - lowest);
+                        sumtoppfactors.set(i, sumtoppfactors.get(i) - lowest);
+                        sumbottomfactors.set(i,(int)Math.pow(primes.get(i),sumbottomfactors.get(i))); //Step 1 of the long path from an ArrayList of prime factors to an integer
+                        sumtoppfactors.set(i,(int)Math.pow(primes.get(i),sumtoppfactors.get(i)));
+                    }
+                    sumbottom = 1;
+                    sumtop = 1;
+                    while (sumbottomfactors.contains(0)) {
+                        sumbottomfactors.remove((Integer) 0); //Removes the 0s from the list so that they don't mess up the calculation
+                    }
+                    while (sumtoppfactors.contains(0)) {
+                        sumtoppfactors.remove((Integer) 0);
+                    }
+                    for (int bottom : sumbottomfactors) {
+                        sumbottom *= bottom; //Reconstructs the prime factors so that they are integers
+                    }
+                    for (int top : sumtoppfactors) {
+                        sumtop *= top;
+                    }
+
+                } else {
+                    sumbottom = num;
+                    sumtop++;
+                }
+
+                System.out.println("Your sum is: " + sumtop + " / " + sumbottom);
+            }
+            if (num == 0) {
+                System.out.println("Do you want to quit? 0 to quit");
+                num = input.nextInt();
+            }
+        } while (num != 0 && times != 10); //The limit of 10 is 100% artificial; this program can do it until the integers overflow
+    }
+
+    private static void  LCMGCD() {
+        System.out.println("Enter your first number: ");
+        int firstnum = input.nextInt();
+        System.out.println("Enter your second number: ");
+        int secondnum = input.nextInt();
+        ArrayList<Integer> primes = new ArrayList<>();
+        primes.add(2);
+        primes.add(3); //Starting prime numbers
+        int highest;
+        int lowest;
+        while (firstnum > primes.get(primes.size()-1) || secondnum > primes.get(primes.size()-1)) {
+            primes = primefinder(primes);
+        }
+        ArrayList<Integer> firstnumfac = getprimefactors(primes,firstnum);
+        ArrayList<Integer> secondnumfac = getprimefactors(primes,secondnum);
+        ArrayList<Integer> lcmnum = new ArrayList<>();
+        ArrayList<Integer> gcdnum = new ArrayList<>();
+        int lcm = 1;
+        int gcd = 1;
+        for (int i = 0; i < primes.size(); i++) {
+            highest = Math.max(firstnumfac.get(i), secondnumfac.get(i)); //Makes sure that this collects the minimum amount of prime factors required for a gcm
+            lcmnum.add(highest);
+            lcmnum.set(i,(int)Math.pow(primes.get(i),lcmnum.get(i))); //Step 1 of the long path from an ArrayList of prime factors to an integer
+        }
+        while (lcmnum.contains(0)) {
+            lcmnum.remove((Integer) 0);
+        }
+        for (int bottom : lcmnum) {
+            lcm *= bottom; //Reconstructs the prime factors so that they are integers
+        }
+        for (int i = 0; i < primes.size(); i++) {
+            lowest = Math.min(firstnumfac.get(i), secondnumfac.get(i)); //Makes sure that this collects the minimum amount of prime factors required for a gcm
+            gcdnum.add(lowest);
+            gcdnum.set(i,(int)Math.pow(primes.get(i),gcdnum.get(i))); //Step 1 of the long path from an ArrayList of prime factors to an integer
+        }
+        while (lcmnum.contains(0)) {
+            gcdnum.remove((Integer) 0);
+        }
+        for (int bottom : gcdnum) {
+            gcd *= bottom; //Reconstructs the prime factors so that they are integers
+        }
+        System.out.println("The GCD of the two numbers is: " + gcd);
+        System.out.println("The LCM of the two numbers is: " + lcm);
+
+    }
+
+    private static ArrayList<Integer> primefinder(ArrayList<Integer> primes) { //This function is needed so that we can correctly reduce the rocepricals
+        int found = 0;
+        int start = (int) primes.get(primes.size()-1);
+        int result;
+        while (found == 0) {
+            start += 2;
+            for (int prime : primes) {
+                if (start % prime == 0) {
+                    found = 0;
+                } else {
+                    found = start;
+                }
+
+            }
+            if (found != 0) {
+                primes.add(found);
+            }
+        }
+        return primes;
+    }
+    private static ArrayList<Integer> getprimefactors(ArrayList<Integer> primes,int tested) {
+        ArrayList<Integer> result = new ArrayList<>(primes);
+        for (int i = 0; i < result.size(); i++)  {
+            result.set(i,0);
+        }
+        for(int i = 0; i < primes.size(); i++) {
+            if (tested % primes.get(i) == 0) {
+                result.set(i,result.get(i)+1);
+                tested /= primes.get(i);
+                i--;
+            }
+        }
+        return result;
+    }
+
+
 }
