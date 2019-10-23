761bc18d0e051bc31ce25e3acebb034687b62f40
Otis Root <otis@otisroot.com>
2019-10-23 13:37:53 -0700

    dsa

--- 4/tempCodeRunnerFile.java
+++ 4/tempCodeRunnerFile.java
@@ -5,3 +5,19 @@ while (bitch % shit != 0 && fuck % shit != 0) {
   shit--;
 }
 
+
+
+String[] uhoh = {"s","t","i","n","k","y"};
+
+System.out.println(uhoh[2]);
+
+for (int poop = 0; poop < uhoh.length; poop++) {
+  System.out.print(uhoh[poop]);
+}
+
+private static int shit(int fuck, int bitch) {
+  int incel = fuck * bitch;
+  return incel;
+}
+
+10 = shit(5,2);
